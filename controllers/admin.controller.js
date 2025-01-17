e20f54f457ec30e309740ba73949ea325fb4f668
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2025-01-17 18:38:49 +0530

    Create admin controller

---
+++ controllers/admin.controller.js
@@ -0,0 +1,16 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { Admin } from "../models/admin.model";
+import bcrypt from "bcrypt"
+
+
+//Register admin
+
+//login admin
+
+//create course
+
+//show created course
+
+//update course
+
+//delete course
