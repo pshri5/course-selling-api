5a1f6d177cd7cae9acb0d244544096935436ffc8
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2025-01-20 11:40:21 +0530

    Update admin controller

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -4,8 +4,37 @@ import bcrypt from "bcrypt"
 
 
 //Register admin
+const registerAdmin = asyncHandler(async(req,res)=>{
+    try {
+        const {firstName, lastName, email, password} = req.body
+        if ([firstName,lastName,email,password].some((field)=>field?.trim()==="")) {
+            throw new Error("All fields are compulsory")
+        }
+        //check if admin already exists
+        const existingAdmin = Admin.findOne({email})
+        if (existingAdmin) {
+            throw new Error("Profile exists")
+        }
+        const admin = await Admin.create({
+            firstName,
+            lastName,
+            email,
+            password
+        })
 
+    } catch (error) {
+        throw new Error("Signup failed")
+    }
+})
 //login admin
+const loginAdmin = asyncHandler(async(req,res)=>{
+    try {
+        const {email, password} = req.body
+        
+    } catch (error) {
+        throw new Error("Signin failed")
+    }
+})
 
 //create course
 
@@ -14,3 +43,10 @@ import bcrypt from "bcrypt"
 //update course
 
 //delete course
+
+
+
+export {
+    registerAdmin,
+    loginAdmin,
+}
