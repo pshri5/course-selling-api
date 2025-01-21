5654d3f5fb19971f2bd661d544f3eed0dc72c9ef
Preetam Shri <110012624+pshri5@users.noreply.github.com>
2025-01-21 09:49:41 +0530

    update admin controller

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -3,6 +3,7 @@ import { Admin } from "../models/admin.model";
 import bcrypt from "bcrypt"
 
 
+
 //Register admin
 const registerAdmin = asyncHandler(async(req,res)=>{
     try {
@@ -30,19 +31,51 @@ const registerAdmin = asyncHandler(async(req,res)=>{
 const loginAdmin = asyncHandler(async(req,res)=>{
     try {
         const {email, password} = req.body
+        if (!email && !password) {
+            throw new Error("Please provide email and password")
+        }
+
+        const admin = Admin.findOne({email})
+        if (!admin) {
+            throw new Error("User does not exist")
+        }
+        const isPasswordCorrect = bcrypt.compare(password, admin.password)
+        if (!isPasswordCorrect) {
+            throw new Error("Invalid credentials")
+        }
+        const accessToken = jwt.sign({_id: admin._id},process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:"15m"
+        })
+        const refreshToken = jwt.sign({_id:admin._id},process.env.REFRESH_TOKEN_SECRET,{
+            expiresIn:"3d"
+        })
+        res.cookie("refreshToken",refreshToken,{
+            httpOnly: true,
+            maxAge: 3 * 24 * 60 * 60 * 1000 //3days
+        })
         
+        res.status(200).json({
+            _id: admin._id,
+            firstName: admin.firstName,
+            lastName: admin.lastName,
+            email: admin.email,
+            accessToken,
+            message: "login Successful"
+        })
+
+
     } catch (error) {
         throw new Error("Signin failed")
     }
 })
 
-//create course
 
-//show created course
 
-//update course
 
-//delete course
+
+
+
+
 
 
 
